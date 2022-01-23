e11df5308adf6e844fa9b2de4274bb1377c95694
Mohanish-9582 <98272016+Mohanish-9582@users.noreply.github.com>
2022-01-23 21:12:41 +0530

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,15 @@
+/*
+Name:- Mohanish Ashok Khambadkar
+Batch:- G3 
+Roll Number:- 764
+Problem Statement:-Write a program in C to display “Hello World”.
+*/
+
+#include<stdio.h>/*#include is a preprocessor directive. Looks for the stdio.h header file.
+stdio.h is a header file which consist of Standard Input Output functions e.g. printf().*/
+
+void main()//main is an entry point of execution.
+{ 
+	printf("Hello World"); //printf is use to display Hello World on screen.
+			
+}//end of the program.
